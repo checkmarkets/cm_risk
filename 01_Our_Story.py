a3426b954c62b67ee34df4df441172e58edc2498
checkmarkets <125349000+checkmarkets@users.noreply.github.com>
2023-02-20 20:32:32 +0100

    Update 01_Our_Story.py

--- 01_Our_Story.py
+++ 01_Our_Story.py
@@ -8,20 +8,20 @@ st.set_page_config(page_title = "check.markets Risk Analysis",
 	#,layout="wide",
 	)
 
-st.image('casino_coverphoto.jpeg')
+#st.image('casino_coverphoto.jpeg')
 
 
 ### CONTENT ###
-st.title("DO YOU HATE RISKY INVESTMENTS THAT ARE OFFERED BY THE MEDIA OR NEWSLETTERS?")
-st.subheader("Look no further than our cutting-edge risk analysis for US stocks with check.markets!")
-st.write("**This app is powered by nothing more than the innovative power of Streamlit. With exclusive access to market and fundamental data, check.markets will now be your favorite go-to place for profound risk analysis for US stocks.**")
+st.title("You love to minimize risk better than anybody else?")
+st.subheader("Then better check out this website. With check.markets you learn how much money you REALLY risk when investing in US stocks.")
+st.write("**Empowered by exclusive access to market and fundamental data, check.markets will now better be your favorite go-to place for profound risk analysis for US stock investments.**")
 
 risk, visuals, ride = st.columns(3)
-risk.write("Whether you're analyzing market trends, predicting potential risks, or simply exploring the data, this app is the ultimate tool for data-driven decision making.")
-visuals.write("With its interactive and visually appealing interface, you can quickly and easily dive into complex data sets and uncover insights that were previously hidden.") 
-ride.write("**So if you're ready to take your risk analysis to the next level, let's go.**")
+risk.write("Whether you're analyzing market trends, predicting potential risks, or simply exploring the data, this app is **the ultimate tool for data-driven decision making**.")
+visuals.write("With its interactive and visually appealing interface, you can quickly and easily dive into complex data sets and uncover insights that you can't find anywhere else.") 
+ride.write("**So if you're ready to take your risk analysis to the next level, add this website to your bookmarks!**")
 
-st.write("**Enter a Stock Ticker on the lefthand side.** If you want to know the Ticker of a specific stock (try **AAPL.US** for Apple or **MSFT.US** for Microsoft), use List of Stocks on the left hand side.")
+st.subheader("**Enter a Stock Ticker on the lefthand side.** Try **AAPL.US** for Apple or **MSFT.US** for Microsoft if you are here for the first time. If you want to know the Ticker of a specific stock, use List of Stocks on the lefthand side.")
 
 with st.sidebar:
 	if "ticker" not in st.session_state:
