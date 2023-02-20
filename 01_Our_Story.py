b0d0b9b571b564140a7f46202b6b7a2c78d48ea4
checkmarkets <125349000+checkmarkets@users.noreply.github.com>
2023-02-20 20:03:12 +0100

    Update 01_Our_Story.py

--- 01_Our_Story.py
+++ 01_Our_Story.py
@@ -2,44 +2,26 @@ import pandas as pd
 import streamlit as st
 
 
-###############     F O R M A T T I N G    #################
-
-
+### FORMATTING ###
 st.set_page_config(page_title = "check.markets Risk Analysis", 
 	page_icon="💲"
 	#,layout="wide",
 	)
 
-
-### LOGO POSITIONING ####
-
 st.image('casino_coverphoto.jpeg')
 
 
-#### NAVIGATION BAR ###
-
-#selected = option_menu(
-#	menu_title="check.markets Risk Dashboard",
-#	options=["Home", "Technical", "Financials", "Risk", "News"],
-#	icons = ["bank", "pie-chart", "bar-chart-line", "ui-checks", "newspaper"],
-#	orientation = "horizontal"
-#	)
-	
-st.title("Are you tired of leaving your investments to chance? With check.markets, this is over now.")
-st.subheader("Look no further than our cutting-edge risk analysis tool for US stocks!")
-
-
-
-st.write("**This app is powered by nothing more than the cutting-edge technology of Streamlit. With exclusive access to market and fundamental data, this application will now be your favorite go-to place for US stock analysis.**")
+### CONTENT ###
+st.title("Do you want to know the REAL risks of your US stock investments?")
+st.subheader("Look no further than our cutting-edge risk analysis for US stocks!")
+st.write("**This app is powered by nothing more than the innovative power of Streamlit. With exclusive access to market and fundamental data, check.markets will now be your favorite go-to place for US stock analysis.**")
 
 risk, visuals, ride = st.columns(3)
-
 risk.write("Whether you're analyzing market trends, predicting potential risks, or simply exploring the data, this app is the ultimate tool for data-driven decision making.")
 visuals.write("With its interactive and visually appealing interface, you can quickly and easily dive into complex data sets and uncover insights that were previously hidden.") 
 ride.write("**So if you're ready to take your risk analysis to the next level, let's go.**")
 
-st.subheader("Start analyzing stocks by entering a Stock Ticker on the left. Get inspired by clicking **'List of Available Stocks'** to check out all stocks you can analyze with this app.")
-
+st.write("**Enter a Stock Ticker on the lefthand side.** If you want to know the Ticker of a specific stock (like "**AAPL.US**" for Apple or "**MSFT.US**" for Microsoft), use "List of Stocks" on the left hand side.")
 
 with st.sidebar:
 	if "ticker" not in st.session_state:
@@ -51,6 +33,4 @@ with st.sidebar:
 		st.session_state["ticker"] = ticker
 		st.write("You are analyzing: ", ticker)
     	
-
-
 ticker_iex = ticker.replace('.US', '')
