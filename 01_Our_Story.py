9d774467295e7346b0e56dc0d6d4663cee113340
checkmarkets <125349000+checkmarkets@users.noreply.github.com>
2023-02-20 21:12:23 +0100

    Update 01_Our_Story.py

--- 01_Our_Story.py
+++ 01_Our_Story.py
@@ -14,16 +14,10 @@ st.image('casino_coverphoto.jpeg')
 ### CONTENT ###
 st.title("You love to minimize risk better than anybody else?")
 st.subheader("Then better check out this website. With check.markets you learn how much money you REALLY risk when investing in US stocks.")
-st.write("**Empowered by exclusive access to market and fundamental data, check.markets will now better be your favorite go-to place for profound risk analysis for US stock investments.**")
+st.write("**Empowered by exclusive access to market and fundamental data, check.markets will now better be your favorite go-to place for profound risk analysis for US stock investments.** So if you're ready to take your risk analysis to the next level, add this website to your bookmarks!")
 
 st.subheader("**Enter a Stock Ticker on the left-hand side.**")
 st.write("If you want to know the Ticker of a specific stock, go to **List of Stocks** on the lefthand side.")
-
-risk, visuals, ride = st.columns(3)
-risk.write("Whether you're analyzing market trends, predicting potential risks, or simply exploring the data, this app is **the ultimate tool for data-driven decision making**.")
-visuals.write("With its interactive and visually appealing interface, you can quickly and easily dive into complex data sets and uncover insights that you can't find anywhere else.") 
-ride.write("**So if you're ready to take your risk analysis to the next level, add this website to your bookmarks!**")
-
 
 
 with st.sidebar:
