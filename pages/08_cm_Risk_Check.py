ed676413611ee9b89b47d7d5b6f457758e9cd231
checkmarkets <125349000+checkmarkets@users.noreply.github.com>
2023-02-27 02:13:09 +0100

    Update 08_cm_Risk_Check.py

--- pages/08_cm_Risk_Check.py
+++ pages/08_cm_Risk_Check.py
@@ -13,14 +13,6 @@ st.set_page_config(page_title = "cm Risk Check",
 eod_api = st.secrets["eod_api"]
 secret_key = st.secrets["secret_key"]
 
-### LOGO POSITIONING ####
-col1, col2, col3 = st.columns(3)
-with col1:
-    st.write('')
-with col2:
-    st.image('LOGO.jpg')
-with col3:
-    st.write('')
 
 st.title("CHECK.MARKETS RISK CHECK")
 st.write("Our intuitive interface provides real-time updates and clear visualizations to help you make informed decisions and stay ahead of market trends.")
