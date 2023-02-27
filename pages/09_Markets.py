931eb6a8c7d19c4b868b5eded365098c7c8ab1b9
checkmarkets <125349000+checkmarkets@users.noreply.github.com>
2023-02-27 01:11:20 +0100

    Update 09_Markets.py

--- pages/09_Markets.py
+++ pages/09_Markets.py
@@ -17,11 +17,11 @@ c = pyEX.Client(secret_key)
 with st.sidebar:
 	if "ticker" not in st.session_state:
 		st.session_state["ticker"] = ""
-	ticker = st.text_input("Enter a Ticker here", st.session_state["ticker"])
+	ticker = st.text_input("Enter a Ticker here (Try **AAPL.US** or **MSFT.US**)", st.session_state["ticker"]).upper()
 	submit = st.button("Submit")
 	
 	if submit:
-		st.session_state["ticker"] = ticker
+		st.session_state["ticker"] = ticker.upper()
 		st.write("You are analyzing: ", ticker)
     	
 ticker_iex = ticker.replace('.US', '')
