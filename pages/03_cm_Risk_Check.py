ec5787f0369ed0ee7a28f3dee4ec5e6845b1ed4c
checkmarkets <125349000+checkmarkets@users.noreply.github.com>
2023-02-27 01:20:37 +0100

    Update 03_cm_Risk_Check.py

--- pages/03_cm_Risk_Check.py
+++ pages/03_cm_Risk_Check.py
@@ -24,7 +24,7 @@ with col3:
     st.write('')
 
 st.title("CHECK.MARKETS RISK CHECK")
-st.subheader("Our intuitive interface provides real-time updates and clear visualizations to help you make informed decisions and stay ahead of market trends.")
+st.write("Our intuitive interface provides real-time updates and clear visualizations to help you make informed decisions and stay ahead of market trends.")
 st.write("Let's start with the most popular Risk KPIs. We compare the actuals to the target ratio of each KPI.")
 
 with st.sidebar:
@@ -65,15 +65,19 @@ crises_df = yf.download(crises_list, interval = "1d")["Adj Close"]
 #MAX DRAWDOWN
 st.write("")
 st.write("")
-st.subheader("Maximum Drawdown Chart ("+ticker+" vs. S&P 500)")
-maxDDDataset = crises_df.dropna(axis=0)
-maxDDChart = ((maxDDDataset.div(maxDDDataset.expanding().max())-1)*100)
-maxDD = ((maxDDDataset.div(maxDDDataset.expanding().max())-1)*100).min()
-maxDD_stock = maxDD[0]
-maxDD_index = maxDD[1]
 
-st.write("The maximum Drawdown of " + ticker_iex + " is at " + str(round(maxDD_stock, 1))+"%" + ", whereas the S&P 500's highest drawdown in this timeframe is at " + str(round(maxDD_index, 1))+"%"  + ". ")
-st.line_chart(maxDDChart)
+tab1, tab2, tab3, tab4 = st.tabs(["Maximum Drawdown", "Risk Dashboard", "Inverted Yield Curve", "Financial Crises Analysis"])
+
+with tab1:
+	st.subheader("Maximum Drawdown Chart ("+ticker+" vs. S&P 500)")
+	maxDDDataset = crises_df.dropna(axis=0)
+	maxDDChart = ((maxDDDataset.div(maxDDDataset.expanding().max())-1)*100)
+	maxDD = ((maxDDDataset.div(maxDDDataset.expanding().max())-1)*100).min()
+	maxDD_stock = maxDD[0]
+	maxDD_index = maxDD[1]
+
+	st.write("The maximum Drawdown of " + ticker_iex + " is at " + str(round(maxDD_stock, 1))+"%" + ", whereas the S&P 500's highest drawdown in this timeframe is at " + str(round(maxDD_index, 1))+"%"  + ". ")
+	st.line_chart(maxDDChart)
 
 days_year = len(df)
 
@@ -136,95 +140,96 @@ volatility = round(np.sqrt(252)*std_dev*100, 1)
 
 ###########################################################################
 
-st.subheader("cm RISK DASHBOARD")
-st.write("")
+with tab2:
+	st.subheader("cm RISK DASHBOARD")
+	st.write("")
 
-sharpe, sortino, treynor = st.columns(3)
-sharpe.metric(label = "Sharpe Ratio (1Y)", value = sharpeRatio, delta = round(sharpeRatio - 1, 1))
-sortino.metric(label = "Sortino Ratio (1Y)", value = sortinoRatio, delta = round(sortinoRatio - 2,1))
-treynor.metric(label = "Treynor Ratio (1Y)", value = treynorRatio, delta = round(treynorRatio - 1,2))
+	sharpe, sortino, treynor = st.columns(3)
+	sharpe.metric(label = "Sharpe Ratio (1Y)", value = sharpeRatio, delta = round(sharpeRatio - 1, 1))
+	sortino.metric(label = "Sortino Ratio (1Y)", value = sortinoRatio, delta = round(sortinoRatio - 2,1))
+	treynor.metric(label = "Treynor Ratio (1Y)", value = treynorRatio, delta = round(treynorRatio - 1,2))
 
-beta1y, corr, vola = st.columns(3)
-beta1y.metric(label = "Beta (1Y)", value = round(beta,2), delta = round(beta - 1,2), delta_color = "inverse")
-corr.metric(label = "Correlation (1Y)", value = str(round(correlation*100,1))+"%", delta = str(round((correlation - 0.5)*100,1))+"%", delta_color = "inverse")
-vola.metric(label = "Volatility (1Y)", value = str(volatility)+"%", delta = str(round(volatility-20, 1))+"%", delta_color = "inverse")
+	beta1y, corr, vola = st.columns(3)
+	beta1y.metric(label = "Beta (1Y)", value = round(beta,2), delta = round(beta - 1,2), delta_color = "inverse")
+	corr.metric(label = "Correlation (1Y)", value = str(round(correlation*100,1))+"%", delta = str(round((correlation - 0.5)*100,1))+"%", delta_color = "inverse")
+	vola.metric(label = "Volatility (1Y)", value = str(volatility)+"%", delta = str(round(volatility-20, 1))+"%", delta_color = "inverse")
 
-st.write("")
+	st.write("")
 
-with st.expander("SHARPE RATIO"):
-	st.write("The **Sharpe Ratio** is a measure of risk-adjusted return that compares an investment's excess return to its volatility (= risk). The higher it is, the better. Typically, a Sharpe Ratio of 1 or higher is considered good, and a ratio of 2 or higher is considered excellent.")
+	with st.expander("SHARPE RATIO"):
+		st.write("The **Sharpe Ratio** is a measure of risk-adjusted return that compares an investment's excess return to its volatility (= risk). The higher it is, the better. Typically, a Sharpe Ratio of 1 or higher is considered good, and a ratio of 2 or higher is considered excellent.")
 
-with st.expander("SORTINO RATIO"):
-	st.write("The **Sortino Ratio** concentrates on the downside risk of a portfolio and is therefore widely appreciated. We compare the actual return of the stock to the real CAGR of the reference index since its inception. Sortino Ratio of 1 or higher is considered adequate, while a ratio of 2 or higher is considered strong.")
+	with st.expander("SORTINO RATIO"):
+		st.write("The **Sortino Ratio** concentrates on the downside risk of a portfolio and is therefore widely appreciated. We compare the actual return of the stock to the real CAGR of the reference index since its inception. Sortino Ratio of 1 or higher is considered adequate, while a ratio of 2 or higher is considered strong.")
 
-with st.expander("TREYNOR RATIO"):
-	st.write("The **Treynor Ratio** (commonly known as reward-to-volatility ratio) is a measure of risk-adjusted return that compares an investment's excess return to the systematic risk of the portfolio, as measured by its beta. Treynor Ratio of 1 or higher is considered satisfactory, while a ratio of 2 or higher is considered remarkable.")
+	with st.expander("TREYNOR RATIO"):
+		st.write("The **Treynor Ratio** (commonly known as reward-to-volatility ratio) is a measure of risk-adjusted return that compares an investment's excess return to the systematic risk of the portfolio, as measured by its beta. Treynor Ratio of 1 or higher is considered satisfactory, while a ratio of 2 or higher is considered remarkable.")
 
-with st.expander("BETA"):
-	st.write("**Beta** indicates how much the stock's price tends to move in response to changes in the market. A beta of 1 means the stock's price moves in line with the market, while a beta greater than 1 indicates the stock is more volatile than the market, and a beta less than 1 indicates the stock is less volatile than the market.")
-with st.expander("CORRELATION"):
-	st.write("If an investor is looking for high-growth stocks or for stocks that move independently from market swings (especially interesting in case of market downturns), they may be willing to accept a higher level of volatility and less **correlation** with the market. In case of upward trends, correlation with the general market is a good sign for stocks.")
-with st.expander("VOLATILITY"):
-	st.write("**Volatility** is a statistical measure of the degree of fluctuation in a stock's price, reflecting the level of risk associated with the investment. For a retail investor with a moderate risk tolerance, a reasonable level of volatility for a stock within a year might be in the range of 10-20%. This would mean that the stock's price could fluctuate by up to 10-20% in either direction over the course of a year.")
-
-st.write("")
-st.write("")
+	with st.expander("BETA"):
+		st.write("**Beta** indicates how much the stock's price tends to move in response to changes in the market. A beta of 1 means the stock's price moves in line with the market, while a beta greater than 1 indicates the stock is more volatile than the market, and a beta less than 1 indicates the stock is less volatile than the market.")
+	with st.expander("CORRELATION"):
+		st.write("If an investor is looking for high-growth stocks or for stocks that move independently from market swings (especially interesting in case of market downturns), they may be willing to accept a higher level of volatility and less **correlation** with the market. In case of upward trends, correlation with the general market is a good sign for stocks.")
+	with st.expander("VOLATILITY"):
+		st.write("**Volatility** is a statistical measure of the degree of fluctuation in a stock's price, reflecting the level of risk associated with the investment. For a retail investor with a moderate risk tolerance, a reasonable level of volatility for a stock within a year might be in the range of 10-20%. This would mean that the stock's price could fluctuate by up to 10-20% in either direction over the course of a year.")
 
 
 #INVERTED YIELD CURVE
+with tab3:
+	st.subheader("(INVERTED) YIELD CURVE")
+	st.write("")
+	with st.expander("EXPLANATION"):
+		st.write("An **Inverted (or Negative) Yield Curve** occurs when short-term treasury yields are higher than long-term treasury yield of the same credit risk profile. Usually, the US treasury bonds are analyzed. While investors mostly take the 2YR and the 10Y Treasury Yield as indicators, academic studies of the relationship between an inverted yield curve and recessions have tended to look at the spread between the yields on the 10YR U.S. Treasury bond and the 3M Treasury bill.")
+	list_of_bonds = ["^IRX", "^TNX"]
+	bonds = yf.download(list_of_bonds, interval="1d")["Adj Close"].dropna(axis=0)
+	short_bond = round(bonds.iloc[-1][0], 2)
+	long_bond = round(bonds.iloc[-1][1], 2)
 
-st.subheader("(INVERTED) YIELD CURVE")
-st.write("")
-with st.expander("EXPLANATION"):
-	st.write("An **Inverted (or Negative) Yield Curve** occurs when short-term treasury yields are higher than long-term treasury yield of the same credit risk profile. Usually, the US treasury bonds are analyzed. While investors mostly take the 2YR and the 10Y Treasury Yield as indicators, academic studies of the relationship between an inverted yield curve and recessions have tended to look at the spread between the yields on the 10YR U.S. Treasury bond and the 3M Treasury bill.")
-list_of_bonds = ["^IRX", "^TNX"]
-bonds = yf.download(list_of_bonds, interval="1d")["Adj Close"].dropna(axis=0)
-short_bond = round(bonds.iloc[-1][0], 2)
-long_bond = round(bonds.iloc[-1][1], 2)
+	st.write("")
+	st.write("")
+	m3, y10, spread = st.columns(3)
+	m3.metric(label = "3M Treasury Yield", value = short_bond)
+	y10.metric(label = "10Y Treasury Yield", value = long_bond)
+	spread.metric(label = "Yield Spread", value = round(long_bond - short_bond,2))
+	st.write("")
+	yield_diff = (bonds["^TNX"] - bonds["^IRX"]).dropna(axis=0)
+	bonds.columns = ["3M Treasury Yd", "10Y Treasury Yd"]
+	st.line_chart(bonds)
 
-st.write("")
-st.write("")
-m3, y10, spread = st.columns(3)
-m3.metric(label = "3M Treasury Yield", value = short_bond)
-y10.metric(label = "10Y Treasury Yield", value = long_bond)
-spread.metric(label = "Yield Spread", value = round(long_bond - short_bond,2))
-st.write("")
-yield_diff = (bonds["^TNX"] - bonds["^IRX"]).dropna(axis=0)
-bonds.columns = ["3M Treasury Yd", "10Y Treasury Yd"]
-st.line_chart(bonds)
+	st.subheader("Yield Spread between 3M and 10Y Treasury Yield")
+	st.write("")
+	st.bar_chart(yield_diff)
 
-st.subheader("Yield Spread between 3M and 10Y Treasury Yield")
-st.write("")
-st.bar_chart(yield_diff)
 
+	
 #CRISES ANALYSIS
-c01 = ["2000-03-24", "2002-10-09"]
-c02 = ["2007-10-09", "2013-03-28"]
-c03 = ["2020-02-19", "2020-08-18"]
-c04 = ["2022-01-03", today]
+with tab4:
+	c01 = ["2000-03-24", "2002-10-09"]
+	c02 = ["2007-10-09", "2013-03-28"]
+	c03 = ["2020-02-19", "2020-08-18"]
+	c04 = ["2022-01-03", today]
 
-st.write("")
-st.write("")
+	st.write("")
+	st.write("")
 
-st.header("HOW DOES " + ticker_iex + " PERFORM DURING CRASHS?")
-st.write("By analyzing how individual stocks or the market as a whole have performed during past crashes, you can gain a better understanding of their potential risk and return characteristics.")
+	st.header("HOW DOES " + ticker_iex + " PERFORM DURING CRASHS?")
+	st.write("By analyzing how individual stocks or the market as a whole have performed during past crashes, you can gain a better understanding of their potential risk and return characteristics.")
 
-st.subheader("DOTCOM BUBBLE BURST (" + c01[0] + " to " + c01[1] + ")")
-norm_c01 = crises_df[c01[0]:c01[1]]
-norm_c01 = norm_c01.div(norm_c01.iloc[0])*100-100
-st.line_chart(norm_c01)
+	st.subheader("DOTCOM BUBBLE BURST (" + c01[0] + " to " + c01[1] + ")")
+	norm_c01 = crises_df[c01[0]:c01[1]]
+	norm_c01 = norm_c01.div(norm_c01.iloc[0])*100-100
+	st.line_chart(norm_c01)
 
-st.subheader("SUBPRIME CRISES (" + c02[0] + " to " + c02[1] + ")")
-norm_c02 = crises_df[c02[0]:c02[1]]
-norm_c02 = norm_c02.div(norm_c02.iloc[0])*100-100
-st.line_chart(norm_c02)
+	st.subheader("SUBPRIME CRISES (" + c02[0] + " to " + c02[1] + ")")
+	norm_c02 = crises_df[c02[0]:c02[1]]
+	norm_c02 = norm_c02.div(norm_c02.iloc[0])*100-100
+	st.line_chart(norm_c02)
 
-st.subheader("CORONA CRISES (" + c03[0] + " to " + c03[1] + ")")
-norm_c03 = crises_df[c03[0]:c03[1]]
-norm_c03 = norm_c03.div(norm_c03.iloc[0])*100-100
-st.line_chart(norm_c03)
+	st.subheader("CORONA CRISES (" + c03[0] + " to " + c03[1] + ")")
+	norm_c03 = crises_df[c03[0]:c03[1]]
+	norm_c03 = norm_c03.div(norm_c03.iloc[0])*100-100
+	st.line_chart(norm_c03)
 
-st.subheader("INFLATION AND INTEREST RATE HIKES (" + c04[0] + " to " + c04[1] + ")")
-norm_c04 = crises_df[c04[0]:c04[1]]
-norm_c04 = norm_c04.div(norm_c04.iloc[0])*100-100
-st.line_chart(norm_c04)
+	st.subheader("INFLATION AND INTEREST RATE HIKES (" + c04[0] + " to " + c04[1] + ")")
+	norm_c04 = crises_df[c04[0]:c04[1]]
+	norm_c04 = norm_c04.div(norm_c04.iloc[0])*100-100
+	st.line_chart(norm_c04)
