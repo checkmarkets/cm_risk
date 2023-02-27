5e3ca7c55bf4e6fc895d87416f65b6e8b378c4ce
checkmarkets <125349000+checkmarkets@users.noreply.github.com>
2023-02-27 01:10:39 +0100

    Update 07_cm_Technicals.py

--- pages/07_cm_Technicals.py
+++ pages/07_cm_Technicals.py
@@ -36,14 +36,13 @@ start_date = datetime.strptime(start_date,'%Y-%m-%d')
 with st.sidebar:
 	if "ticker" not in st.session_state:
 		st.session_state["ticker"] = ""
-	ticker = st.text_input("Enter a Ticker here", st.session_state["ticker"])
+	ticker = st.text_input("Enter a Ticker here (Try **AAPL.US** or **MSFT.US**)", st.session_state["ticker"]).upper()
 	submit = st.button("Submit")
 	
 	if submit:
-		st.session_state["ticker"] = ticker
+		st.session_state["ticker"] = ticker.upper()
 		st.write("You are analyzing: ", ticker)
-
-
+    	
 ticker_iex = ticker.replace('.US', '')
 
 
