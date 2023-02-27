aef170b7d2d32155af965d7786fddd92f25c4008
checkmarkets <125349000+checkmarkets@users.noreply.github.com>
2023-02-27 01:09:08 +0100

    Update 03_cm_Risk_Check.py

--- pages/03_cm_Risk_Check.py
+++ pages/03_cm_Risk_Check.py
@@ -30,11 +30,11 @@ st.write("Let's start with the most popular Risk KPIs. We compare the actuals to
 with st.sidebar:
 	if "ticker" not in st.session_state:
 		st.session_state["ticker"] = ""
-	ticker = st.text_input("Enter a Ticker here", st.session_state["ticker"])
+	ticker = st.text_input("Enter a Ticker here (Try **AAPL.US** or **MSFT.US**)", st.session_state["ticker"]).upper()
 	submit = st.button("Submit")
 	
 	if submit:
-		st.session_state["ticker"] = ticker
+		st.session_state["ticker"] = ticker.upper()
 		st.write("You are analyzing: ", ticker)
 
 ticker_iex = ticker.replace('.US', '')
